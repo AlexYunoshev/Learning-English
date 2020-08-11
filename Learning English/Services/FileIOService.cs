0be70498b2b738f17b3163e618352151c67b6c8e
Alexander Yunoshev <AlexYunoshev@users.noreply.github.com>
2020-08-11 19:35:40 +0300

    code rewiew 3

--- Learning English/Services/FileIOService.cs
+++ Learning English/Services/FileIOService.cs
@@ -13,9 +13,9 @@ namespace Learning_English.Services
 {
     class FileIOService
     {
-        private readonly string pathWords;
-        private readonly string pathUnits;
-        private readonly string pathStatistic;
+        private readonly string pathWords; /// путь к файлу со словами
+        private readonly string pathUnits; /// путь к файлу с юнитами
+        private readonly string pathStatistic; /// путь к файлу статистики
 
         public FileIOService(string pathWords, string pathUnits, string pathStatistic)
         {
@@ -29,6 +29,10 @@ namespace Learning_English.Services
             this.pathStatistic = pathStatistic;
         }
 
+        /// <summary>
+        /// Метод загрузки слов
+        /// </summary>
+        /// <returns>Возвращает коллекцию BindingList</returns>
         public BindingList<Word> LoadDataWords()
         {
             var fileExists = File.Exists(pathWords);
@@ -41,6 +45,15 @@ namespace Learning_English.Services
             {
                 var fileText = reader.ReadToEnd();
                 return JsonConvert.DeserializeObject<BindingList<Word>>(fileText);
+            }
+        }
+
+        public void SaveDataWords(object English_Data)
+        {
+            using (StreamWriter writer = File.CreateText(pathWords))
+            {
+                string output = JsonConvert.SerializeObject(English_Data);
+                writer.Write(output);
             }
         }
 
@@ -68,24 +81,6 @@ namespace Learning_English.Services
             }
         }
 
-        public void SaveDataWords(object English_Data)
-        {
-            using (StreamWriter writer = File.CreateText(pathWords))
-            {
-                string output = JsonConvert.SerializeObject(English_Data);
-                writer.Write(output);
-            }
-        }
-
-        public void SaveStatisticData(List<int> list)
-        {
-            using (StreamWriter writer = File.CreateText(pathStatistic))
-            {
-                string output = JsonConvert.SerializeObject(list);
-                writer.Write(output);
-            }
-        }
-
         public List<int> LoadStatisticData()
         {
             var fileExists = File.Exists(pathStatistic);
@@ -101,6 +96,14 @@ namespace Learning_English.Services
             }
         }
 
+        public void SaveStatisticData(List<int> list)
+        {
+            using (StreamWriter writer = File.CreateText(pathStatistic))
+            {
+                string output = JsonConvert.SerializeObject(list);
+                writer.Write(output);
+            }
+        }
 
     }
 }
